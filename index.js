5ec8a69b48089f04a6cfa7976a9d9262c0258916
Hande Budak <bdkhande@gmail.com>
2024-03-17 15:41:36 +0300

    js dosyası düzenlendi

--- index.js
+++ index.js
@@ -1,97 +1,3 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
-
 let generateButton = document.getElementById("generate-button");
 
 let randomPasswords = document.querySelectorAll(".password");
@@ -191,17 +97,16 @@ generateButton.addEventListener("click", () => {
     "/",
   ];
 
-  let password = "";
-
-  const charactersLenght = characters.length;
-  const passwordLenght = 12;
-
-  for (let i = 0; i < passwordLenght; i++) {
-    const randomIndex = Math.floor(Math.random() * charactersLenght);
-    password += characters[randomIndex];
-  }
-
   randomPasswords.forEach((passwordElement) => {
+    let password = "";
+    const charactersLenght = characters.length;
+    const passwordLenght = 12;
+
+    for (let i = 0; i < passwordLenght; i++) {
+      const randomIndex = Math.floor(Math.random() * charactersLenght);
+      password += characters[randomIndex];
+    }
+
     passwordElement.textContent = password;
   });
 });
